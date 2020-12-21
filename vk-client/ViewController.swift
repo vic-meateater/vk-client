b2d069a7ce8a19ff4eb90fd3f60116313fa815f7
Vic <meateater.regs@gmail.com>
2020-12-21 22:22:17 +0500

    Initial Commit

---
+++ vk-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-client
+//
+//  Created by Vict on 21.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
